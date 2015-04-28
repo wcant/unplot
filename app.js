e97a1cba5e0b2379c32686d15b838ae3bd1ea61d
Wesley Cantrell <cantrell.wesley@gmail.com>
2015-04-27 23:34:00 -0500

    upload is working well with multer; now to the real problem

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var express = require('express');
-var app = express();
+var multer = require('multer'),
+    express = require('express'),
+    app = express();
+
+app.use(multer({ 
+  dest:'./uploads/',
+  rename: function (fieldname, filename) {
+    return filename.replace(/\W+/g, '-').toLowerCase() + Date.now()
+  } 
+}));
 
 app.use(express.static('public'));
 
@@ -7,4 +15,13 @@ app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
-app.listen(3000);
+app.post('/file-upload', function(req, res, next) {
+  console.log(req.body)
+  console.log(req.files)
+  
+  res.status(204).end()
+});
+
+app.listen(3000, function() {
+  console.log("Listening on 3000...");
+});
