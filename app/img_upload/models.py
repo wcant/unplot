cff3600a4082af7ad0a87f12732f5d12eb54285c
Wesley Cantrell <cantrell.wesley@gmail.com>
2016-07-06 15:45:07 -0500

    changing file structure and building img_upload module; need to setup routes

--- app/img_upload/models.py
+++
@@ -1,5 +0,0 @@
-#Import database object (db) from the main application module
-#which was defined in /app/__init__.py
-from app import db
-
-
---
+++ app/modules/img_upload.py
@@ -0,0 +1,19 @@
+# Import flask dependencies
+from flask import Blueprint, request, render_template, \
+                  flash, g, session, redirect, url_for
+from jinja2 import TemplateNotFound
+
+# Import the database object from the main app module
+from app import db
+
+# Import module models
+#?
+
+# Define the blueprint: 'img_upload', set its url prefix: app.url/img_upload
+img_upload = Blueprint('img_upload', __name__,
+                       template_folder='templates')
+
+# Set the route and accpeted methods
+@img_upload.route('/img_upload', methods=['GET', 'POST'])
+def img_upload():
+    return render_template("/img_upload.html")
