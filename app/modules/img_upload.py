8910527634f86ab835c0b7873039bf47d707d6da
Wesley Cantrell <cantrell.wesley@gmail.com>
2016-07-07 16:41:25 -0500

    backup

--- app/modules/img_upload.py
+++ app/modules/img_upload.py
@@ -10,10 +10,11 @@ from app import db
 #?
 
 # Define the blueprint: 'img_upload', set its url prefix: app.url/img_upload
-img_upload = Blueprint('img_upload', __name__,
-                       template_folder='templates')
+img_upload = Blueprint('img_upload', __name__)
 
 # Set the route and accpeted methods
-@img_upload.route('/img_upload', methods=['GET', 'POST'])
+@img_upload.route('/')
 def img_upload():
     return render_template("/img_upload.html")
+
+
