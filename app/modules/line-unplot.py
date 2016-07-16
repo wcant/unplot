f0341227e9846ce6cedff43e9e327ae46deb3e77
Wesley Cantrell <cantrell.wesley@gmail.com>
2016-07-15 20:08:50 -0500

    need to get the second most common color instead of first

--- app/modules/line-unplot.py
+++ app/modules/line-unplot.py
@@ -2,12 +2,33 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import itertools
+import operator
 
 
 def histogram_plot(data):
 
     plt.hist(data, bins=10, normed=False)
     plt.show()
+
+
+def most_common(data):
+
+    SL = sorted((x, i) for i, x in enumerate(data))
+    groups = itertools.groupby(SL, key=operator.itemgetter(0))
+
+    def _auxfun(g):
+        item, iterable = g
+        count = 0
+        min_index = len(data)
+        for _, where in iterable:
+            count += 1
+            min_index = min(min_index, where)
+
+        return count, -min_index
+
+    return max(groups, key=_auxfun)[0]
+
 
 def main():
 
@@ -23,8 +44,8 @@ def main():
 
     flatarray = bitarray.flatten()
     hexarray = [hex(i) for i in flatarray]
-    
-    histogram_plot(flatarray)
+
+    print(most_common(flatarray))
 
 
 if __name__ == "__main__":
